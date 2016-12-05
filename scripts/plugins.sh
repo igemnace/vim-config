71a29c6e3f54d5867c070d9f3f47f8ed6070f8ab
Ian Emnace <igemnace@gmail.com>
2016-12-05 09:44:57 +0800

    add vim-airline to plugins

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -14,6 +14,9 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 # CTRL-P
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 
+# vim-airline
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
+
 # vim-surround
 git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
 
