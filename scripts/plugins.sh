a952e7022959dec0b5d505e1709bf3301ef4fad5
Ian Emnace <igemnace@gmail.com>
2016-12-13 22:16:38 +0800

    add vim-gitgutter

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -17,6 +17,9 @@ git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 # vim-airline
 git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
 
+# vim-gitgutter
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+
 # vim-surround
 git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
 
