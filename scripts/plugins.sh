739ee51be43c0d6b7866c62cddb6156831e0d543
Ian Emnace <igemnace@gmail.com>
2016-12-19 09:50:15 +0800

    add vim-obsession plugin

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -17,6 +17,9 @@ git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 # vim-airline
 git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
 
+# vim-obsession
+git clone https://github.com/tpope/vim-obsession.git ~/.vim/bundle/vim-obsession
+
 # vim-gitgutter
 git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
 
