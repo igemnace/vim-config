fca3f68f09502c6c1d06c201e02e532991513376
Ian Emnace <igemnace@gmail.com>
2017-03-21 13:45:04 +0800

    change plugin system to vim 8 native

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -3,97 +3,117 @@
 if [ ! -d "$HOME/.vim" ]; then
   mkdir "$HOME/.vim"
 fi
-
-if [ ! -d "$HOME/.vim/bundle" ]; then
-  mkdir "$HOME/.vim/bundle"
+if [ ! -d "$HOME/.vim/pack" ]; then
+  mkdir "$HOME/.vim/pack"
 fi
 
-# NERDTree
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+### CODE
+if [ ! -d "$HOME/.vim/pack/code" ]; then
+  mkdir "$HOME/.vim/pack/code"
+fi
+if [ ! -d "$HOME/.vim/pack/code/start" ]; then
+  mkdir "$HOME/.vim/pack/code/start"
+fi
+if [ ! -d "$HOME/.vim/pack/code/opt" ]; then
+  mkdir "$HOME/.vim/pack/code/opt"
+fi
 
-# fzf.vim
-git clone https://github.com/junegunn/fzf.vim.git ~/.vim/bundle/fzf.vim
+# START
+cd "$HOME/.vim/pack/code/start"
+git clone https://github.com/w0rp/ale.git
+git clone https://github.com/mattn/emmet-vim
+git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/elzr/vim-json.git
+git clone https://github.com/mxw/vim-jsx.git
 
-# vim-airline
-git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
+# OPT
+cd "$HOME/.vim/pack/code/opt"
+git clone https://github.com/freitass/todo.txt-vim.git
+### END CODE
 
-# vim-airline-themes
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+### EXTEND
+if [ ! -d "$HOME/.vim/pack/extend" ]; then
+  mkdir "$HOME/.vim/pack/extend"
+fi
+if [ ! -d "$HOME/.vim/pack/extend/start" ]; then
+  mkdir "$HOME/.vim/pack/extend/start"
+fi
+if [ ! -d "$HOME/.vim/pack/extend/opt" ]; then
+  mkdir "$HOME/.vim/pack/extend/opt"
+fi
 
-# base16-vim
-git clone https://github.com/chriskempson/base16-vim.git ~/.vim/bundle/base16-vim
+# START
+cd "$HOME/.vim/pack/extend/start"
+git clone https://github.com/jiangmiao/auto-pairs.git
+git clone git://github.com/tpope/vim-abolish.git
+git clone https://github.com/FooSoft/vim-argwrap
+git clone https://github.com/tpope/vim-commentary.git
+git clone git://github.com/tpope/vim-repeat.git
+git clone git://github.com/tpope/vim-rsi.git
+git clone https://github.com/tpope/vim-sleuth.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/tpope/vim-unimpaired.git
+git clone https://github.com/maxbrunsfeld/vim-yankstack.git
 
-# ale
-git clone https://github.com/w0rp/ale.git ~/.vim/bundle/ale
+# OPT
+cd "$HOME/.vim/pack/extend/opt"
+# TODO: add gundo
+git clone git://github.com/tpope/vim-speeddating.git
+### END EXTEND
 
-# codi.vim
-git clone https://github.com/metakirby5/codi.vim.git ~/.vim/bundle/codi.vim
+### FILENAV
+if [ ! -d "$HOME/.vim/pack/filenav" ]; then
+  mkdir "$HOME/.vim/pack/filenav"
+fi
+if [ ! -d "$HOME/.vim/pack/filenav/start" ]; then
+  mkdir "$HOME/.vim/pack/filenav/start"
+fi
 
-# vim-slime
-git clone https://github.com/jpalardy/vim-slime.git ~/.vim/bundle/vim-slime
+# START
+cd "$HOME/.vim/pack/filenav/start"
+git clone https://github.com/junegunn/fzf.vim.git
+git clone https://github.com/tpope/vim-vinegar.git
+### END FILENAV
 
-# vim-obsession
-git clone https://github.com/tpope/vim-obsession.git ~/.vim/bundle/vim-obsession
+### INTEGRATE
+if [ ! -d "$HOME/.vim/pack/integrate" ]; then
+  mkdir "$HOME/.vim/pack/integrate"
+fi
+if [ ! -d "$HOME/.vim/pack/integrate/start" ]; then
+  mkdir "$HOME/.vim/pack/integrate/start"
+fi
+if [ ! -d "$HOME/.vim/pack/integrate/opt" ]; then
+  mkdir "$HOME/.vim/pack/integrate/opt"
+fi
 
-# vim-gitgutter
-git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+# START
+cd "$HOME/.vim/pack/integrate/start"
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git
 
-# vim-fugitive
-git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+# OPT
+cd "$HOME/.vim/pack/integrate/opt"
+git clone https://github.com/tpope/vim-eunuch.git
+git clone https://github.com/jpalardy/vim-slime.git
+### END INTEGRATE
 
-# vim-tmux-navigator
-git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/bundle/vim-tmux-navigator
+### THEME
+if [ ! -d "$HOME/.vim/pack/theme" ]; then
+  mkdir "$HOME/.vim/pack/theme"
+fi
+if [ ! -d "$HOME/.vim/pack/theme/start" ]; then
+  mkdir "$HOME/.vim/pack/theme/start"
+fi
+if [ ! -d "$HOME/.vim/pack/theme/opt" ]; then
+  mkdir "$HOME/.vim/pack/theme/opt"
+fi
 
-# vim-eunuch
-git clone https://github.com/tpope/vim-eunuch.git ~/.vim/bundle/vim-eunuch
+# START
+cd "$HOME/.vim/pack/theme/start"
+git clone https://github.com/chriskempson/base16-vim.git
 
-# emmet-vim
-git clone https://github.com/mattn/emmet-vim ~/.vim/bundle/emmet-vim
-
-# vim-surround
-git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
-
-# vim-commentary
-git clone https://github.com/tpope/vim-commentary.git ~/.vim/bundle/vim-commentary
-
-# vim-unimpaired
-git clone https://github.com/tpope/vim-unimpaired.git ~/.vim/bundle/vim-unimpaired
-
-# vim-speeddating
-git clone git://github.com/tpope/vim-speeddating.git ~/.vim/bundle/vim-speeddating
-
-# vim-abolish
-git clone git://github.com/tpope/vim-abolish.git ~/.vim/bundle/vim-abolish
-
-# vim-repeat
-git clone git://github.com/tpope/vim-repeat.git ~/.vim/bundle/vim-repeat
-
-# vim-yankstack
-git clone https://github.com/maxbrunsfeld/vim-yankstack.git ~/.vim/bundle/vim-yankstack
-
-# auto-pairs
-git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs
-
-# vim-argwrap
-git clone https://github.com/FooSoft/vim-argwrap ~/.vim/bundle/vim-argwrap
-
-# vim-rsi
-git clone git://github.com/tpope/vim-rsi.git ~/.vim/bundle/vim-rsi
-
-# vim-sleuth
-git clone https://github.com/tpope/vim-sleuth.git ~/.vim/bundle/vim-sleuth
-
-# todo.txt-vim
-git clone https://github.com/freitass/todo.txt-vim.git ~/.vim/bundle/todo.txt-vim
-
-# vim-javascript
-git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
-
-# vim-json
-git clone https://github.com/elzr/vim-json.git ~/.vim/bundle/vim-json
-
-# vim-jsx
-git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
-
-# vim-css-color
-git clone https://github.com/ap/vim-css-color.git ~/.vim/bundle/vim-css-color
+# OPT
+cd "$HOME/.vim/pack/theme/opt"
+git clone https://github.com/ap/vim-css-color.git
+### END THEME
