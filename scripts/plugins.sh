9fef6dc6d052fd950b745884d357eece2aedbea4
Ian Emnace <igemnace@gmail.com>
2017-05-23 13:45:28 +0800

    make plugin installation async

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
+### FUNCTIONS
+bg_clone() {
+  >&2 echo "Cloning $1..."
+  git clone $1 &>/dev/null &
+}
+
 ### CODE
 mkdir -p "$HOME/.vim/pack/code/start"
 mkdir -p "$HOME/.vim/pack/code/opt"
 
 # START
 cd "$HOME/.vim/pack/code/start"
-git clone https://github.com/elzr/vim-json.git
-git clone https://github.com/freitass/todo.txt-vim.git
-git clone https://github.com/mattn/emmet-vim
-git clone https://github.com/mxw/vim-jsx.git
-git clone https://github.com/pangloss/vim-javascript.git
-git clone https://github.com/w0rp/ale.git
+bg_clone https://github.com/elzr/vim-json.git
+bg_clone https://github.com/freitass/todo.txt-vim.git
+bg_clone https://github.com/mattn/emmet-vim
+bg_clone https://github.com/mxw/vim-jsx.git
+bg_clone https://github.com/pangloss/vim-javascript.git
+bg_clone https://github.com/w0rp/ale.git
+bg_clone https://github.com/vimwiki/vimwiki.git
 
 # OPT
 cd "$HOME/.vim/pack/code/opt"
-git clone https://github.com/dhruvasagar/vim-table-mode.git
-git clone https://github.com/ternjs/tern_for_vim.git
+bg_clone https://github.com/dhruvasagar/vim-table-mode.git
+bg_clone https://github.com/ternjs/tern_for_vim.git
 ### END CODE
 
 ### EXTEND
@@ -25,25 +32,25 @@ mkdir -p "$HOME/.vim/pack/extend/opt"
 
 # START
 cd "$HOME/.vim/pack/extend/start"
-git clone git://github.com/tpope/vim-abolish.git
-git clone git://github.com/tpope/vim-repeat.git
-git clone git://github.com/tpope/vim-rsi.git
-git clone git://github.com/tpope/vim-speeddating.git
-git clone https://github.com/FooSoft/vim-argwrap
-git clone https://github.com/jiangmiao/auto-pairs.git
-git clone https://github.com/maxbrunsfeld/vim-yankstack.git
-git clone https://github.com/skywind3000/asyncrun.vim.git
-git clone https://github.com/tpope/vim-commentary.git
-git clone https://github.com/tpope/vim-dispatch.git
-git clone https://github.com/tpope/vim-endwise
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/tpope/vim-unimpaired.git
+bg_clone git://github.com/tpope/vim-abolish.git
+bg_clone git://github.com/tpope/vim-repeat.git
+bg_clone git://github.com/tpope/vim-rsi.git
+bg_clone git://github.com/tpope/vim-speeddating.git
+bg_clone https://github.com/FooSoft/vim-argwrap
+bg_clone https://github.com/jiangmiao/auto-pairs.git
+bg_clone https://github.com/maxbrunsfeld/vim-yankstack.git
+bg_clone https://github.com/skywind3000/asyncrun.vim.git
+bg_clone https://github.com/tpope/vim-commentary.git
+bg_clone https://github.com/tpope/vim-dispatch.git
+bg_clone https://github.com/tpope/vim-endwise
+bg_clone https://github.com/tpope/vim-surround.git
+bg_clone https://github.com/tpope/vim-unimpaired.git
 
 # OPT
 cd "$HOME/.vim/pack/extend/opt"
-git clone https://github.com/Konfekt/FastFold.git
-git clone https://github.com/sjl/gundo.vim
-git clone https://github.com/tpope/vim-sleuth.git
+bg_clone https://github.com/Konfekt/FastFold.git
+bg_clone https://github.com/sjl/gundo.vim
+bg_clone https://github.com/tpope/vim-sleuth.git
 ### END EXTEND
 
 ### FILENAV
@@ -51,8 +58,8 @@ mkdir -p "$HOME/.vim/pack/filenav/start"
 
 # START
 cd "$HOME/.vim/pack/filenav/start"
-git clone https://github.com/junegunn/fzf.vim.git
-git clone https://github.com/tpope/vim-vinegar.git
+bg_clone https://github.com/junegunn/fzf.vim.git
+bg_clone https://github.com/tpope/vim-vinegar.git
 ### END FILENAV
 
 ### INTEGRATE
@@ -61,16 +68,16 @@ mkdir -p "$HOME/.vim/pack/integrate/opt"
 
 # START
 cd "$HOME/.vim/pack/integrate/start"
-git clone https://github.com/airblade/vim-gitgutter.git
-git clone https://github.com/christoomey/vim-tmux-navigator.git
-git clone https://github.com/tommcdo/vim-fubitive.git
-git clone https://github.com/tpope/vim-eunuch.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/tpope/vim-rhubarb.git
+bg_clone https://github.com/airblade/vim-gitgutter.git
+bg_clone https://github.com/christoomey/vim-tmux-navigator.git
+bg_clone https://github.com/tommcdo/vim-fubitive.git
+bg_clone https://github.com/tpope/vim-eunuch.git
+bg_clone https://github.com/tpope/vim-fugitive.git
+bg_clone https://github.com/tpope/vim-rhubarb.git
 
 # OPT
 cd "$HOME/.vim/pack/integrate/opt"
-git clone https://github.com/jpalardy/vim-slime.git
+bg_clone https://github.com/jpalardy/vim-slime.git
 ### END INTEGRATE
 
 ### THEME
@@ -79,9 +86,12 @@ mkdir -p "$HOME/.vim/pack/theme/opt"
 
 # START
 cd "$HOME/.vim/pack/theme/start"
-git clone https://github.com/chriskempson/base16-vim.git
+bg_clone https://github.com/chriskempson/base16-vim.git
 
 # OPT
 cd "$HOME/.vim/pack/theme/opt"
-git clone https://github.com/ap/vim-css-color.git
+bg_clone https://github.com/ap/vim-css-color.git
 ### END THEME
+
+wait
+>&2 echo "Done."
