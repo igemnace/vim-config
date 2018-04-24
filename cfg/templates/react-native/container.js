ed72486c289d32522ff34ff616d834712d3c701d
Ian Emnace <igemnace@gmail.com>
2018-04-24 14:24:25 +0800

    Update templates
    A simple container.js (almost exact copy of hoc.js) has been added.
    Unfortunately, I've misused the name "HOC" too much in the past to fix
    in the near future, but I can start by properly naming them "containers"
    from here on.
    
    Additionally, I've given in and made pug default in my Vue templates,
    since I only ever use pug and it's gotten tiring to keep manually adding
    lang="pug" to new files.

---
+++ cfg/templates/react-native/container.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React, { PropTypes, Component } from 'react';
+import SkeletonName from './skeleton-name.component.js';
+
+export default class SkeletonNameContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static propTypes = {}
+
+  render() {
+    return (
+      <SkeletonName
+        {...this.props}
+      />
+    );
+  }
+}
