d406a04a59bfb64123947a96be8def50f6286883
Ian Emnace <igemnace@gmail.com>
2020-01-24 21:58:20 +0800

    Add Jest template
    I've chosen to go with a Vue-component-specific template for Jest, since
    this is my main use case nowadays.

---
+++ cfg/templates/jest/component.spec.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import SkeletonName from '../skeleton-name';
+import { mount } from '@vue/test-utils';
+
+describe('SkeletonName', () => {
+  Vue.use(Vuetify);
+
+  describe('props', () => {});
+
+  describe('events', () => {});
+});
