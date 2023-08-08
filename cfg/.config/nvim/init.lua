e997238b95723d65b20a7575c44a9887f579ba2f
Ian Emnace <igemnace@gmail.com>
2023-08-08 18:36:30 +0800

    Tweak nvim/init.lua
    Some straggler options aren't handled nicely in just vimrc:
    
    - 'mouse' default is different, so I set that explicitly
    
    - 'backupdir' I would have wanted to keep Neovim's default for Neovim
      specifically, so it doesn't share with Vim, so I explicitly set it to
      the XDG dir in init.lua (I have to use vim.fn.expand so I don't
      hardcode in ~...)
    
    - <C-@> has to be mapped with a recursive map to <C-Space>, because nvim
      has saner key handling and treats the two differently

--- cfg/.config/nvim/init.lua
+++ cfg/.config/nvim/init.lua
@@ -6,12 +6,22 @@ vim.opt.runtimepath:append { '~/.vim/after' }
 vim.opt.packpath = vim.opt.runtimepath:get()
 
 -- source main vimrc
-vim.cmd('source ~/.vim/vimrc')
+vim.cmd 'source ~/.vim/vimrc'
 
 -- NEOVIM-SPECIFIC --
 
+-- force disable mouse
+vim.opt.mouse = nil
+
+-- force backup dir to be XDG
+vim.opt.backupdir = { vim.fn.expand('~/.local/state/nvim/backup//') }
+
 -- always use a block cursor
 vim.opt.guicursor = { 'a:block' }
+
+-- set up Insert-mode leader (<C-@> is different from <C-Space> on nvim)
+vim.keymap.set('i', '<C-Space>', '<C-@>', { remap = true })
+vim.keymap.set('i', '<C-Space><C-Space>', '<Plug>SnipletExpand', { remap = true })
 
 -- set up mappings for traversing diagnostics
 vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float)
